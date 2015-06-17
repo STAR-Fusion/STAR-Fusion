da4b521d504e1f6a854fbd6e76ee472cf38667e0
Brian Haas <brianjohnhaas@gmail.com>
2015-06-16 21:06:04 -0400

    discard Selenocysteine annotations

--- lib/GTF_utils.pm
+++ lib/GTF_utils.pm
@@ -76,6 +76,10 @@ sub GTF_to_gene_objs {
 
     my %gene_id_to_seq_name;
     my %gene_id_to_gene_name;
+
+
+    my %coding_genes;
+    
     
     open (my $fh, $gtf_filename) or die "Error, cannot open $gtf_filename";
     while (<$fh>) {
@@ -122,9 +126,14 @@ sub GTF_to_gene_objs {
         if ($type eq 'CDS' || $type eq 'stop_codon' || $type eq 'start_codon') {
             push (@{$gene_transcript_data{$seqname}->{$gene_id}->{$transcript_id}->{CDS}}, [$end5, $end3] );
             push (@{$gene_transcript_data{$seqname}->{$gene_id}->{$transcript_id}->{mRNA}}, [$end5, $end3] );
+
+            $coding_genes{$gene_id}++;
         }
         elsif ($type eq "exon" || $type =~ /UTR/) {
             push (@{$gene_transcript_data{$seqname}->{$gene_id}->{$transcript_id}->{mRNA}}, [$end5, $end3] );
+        }
+        elsif ($type =~ /Selenocysteine/) {
+            # no op
         }
         else {
             ## assuming noncoding feature
@@ -138,7 +147,6 @@ sub GTF_to_gene_objs {
     ## create gene objects.
  
     my @top_gene_objs;
-
 
     my %seen;
     foreach my $seqname (keys %gene_transcript_data) {
@@ -236,6 +244,11 @@ sub GTF_to_gene_objs {
                     
                     my $gene_ids_href = $ncgene_types_href->{$nc_type};
                     foreach my $gene_id (keys %$gene_ids_href) {
+
+                        if (exists $coding_genes{$gene_id}) {
+                            print STDERR "Warning: Skipping $gene_id ($nc_type) as this gene is already included as a coding gene.\n";
+                            next;
+                        }
 
                         my $trans_ids_href = $gene_ids_href->{$gene_id};
                         foreach my $trans_id (keys %$trans_ids_href) {
