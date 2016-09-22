d04859f079cf8ac2fda28716cc1ed42f46b5402b
Brian Haas <brianjohnhaas@gmail.com>
2016-09-22 12:29:43 -0400

    adding small test

---
+++ testing/__misc_tests/__BT474/runMe.sh
@@ -0,0 +1,1 @@
+../../../STAR-Fusion --left_fq test.reads_1.fastq.gz --right_fq test.reads_2.fastq.gz --genome_lib_dir /seq/regev_genome_portal/RESOURCES/CTAT_GENOME_LIB/GRCh37_gencode_v19_FL3 -O starF
