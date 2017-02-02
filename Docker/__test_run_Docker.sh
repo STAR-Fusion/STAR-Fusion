f060f26af16dc9a5c50b25ce0f4cd7da9f05a5dc
Ubuntu <ubuntu@ip-172-31-25-151.ec2.internal>
2017-02-02 02:58:54 +0000

    include docker run command for star-fusion

--- Docker/__test_run_Docker.sh
+++ Docker/__test_run_Docker.sh
@@ -2,9 +2,9 @@
 
 set -ve
 
-CTAT_GENOME_LIB="GRCh37_gencode_v19_CTAT_lib_July272016"
+CTAT_GENOME_LIB="GRCh37_gencode_v19_CTAT_lib_July272016_prebuilt"
 
-CTAT_GENOME_LIB_URL="https://data.broadinstitute.org/Trinity/CTAT_RESOURCE_LIB/GRCh37_gencode_v19_CTAT_lib_July272016.tar.gz"
+CTAT_GENOME_LIB_URL="https://data.broadinstitute.org/Trinity/CTAT_RESOURCE_LIB/${CTAT_GENOME_LIB}.tar.gz"
 
 
 if [ ! -s "${CTAT_GENOME_LIB}.tar.gz" ]; then
@@ -20,3 +20,4 @@ VERSION=`cat VERSION.txt`
 
 docker pull trinityctat/ctatfusion:${VERSION}
 
+docker run -v `pwd`/../:/data --rm trinityctat/ctatfusion /usr/local/src/STAR-Fusion-v1.0.0/STAR-Fusion --left_fq /data/testing/rs_eads_1.fq.gz --right_fq /data/testing/reads_2.fq.gz --genome_lib_dir /data/Docker/GRCh37_gencode_v19_CTAT_lib_July272016_prebuilt -O outdir
