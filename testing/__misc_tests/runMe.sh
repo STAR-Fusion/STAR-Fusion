4bcc9630e215c17056366713c0abd38fcf5b26db
Brian Haas <brianjohnhaas@gmail.com>
2017-07-05 11:14:50 -0400

    pass opts to runMe

--- testing/__misc_tests/runMe.sh
+++ testing/__misc_tests/runMe.sh
@@ -7,7 +7,7 @@ fi
 
 ## set genome_lib_dir (see: http://FusionFilter.github.io for details)
 
-../STAR-Fusion --left_fq reads_1.fq.gz --right_fq reads_2.fq.gz -O star_fusion_outdir --genome_lib_dir $CTAT_GENOME_LIB --verbose_level 2
+../STAR-Fusion --left_fq reads_1.fq.gz --right_fq reads_2.fq.gz -O star_fusion_outdir --genome_lib_dir $CTAT_GENOME_LIB --verbose_level 2 $*
 
 
 
