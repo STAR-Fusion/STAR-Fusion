b596261af82bd1bd228a913447b5683e9fffa9c3
brianjohnhaas <brianjohnhaas@gmail.com>
2017-11-03 18:21:28 +0000

    docker updates for latest version

--- Docker/__test_run_Docker.sh
+++ Docker/__test_run_Docker.sh
@@ -2,7 +2,7 @@
 
 set -ve
 
-CTAT_GENOME_LIB="GRCh37_gencode_v19_CTAT_lib_July192017"
+CTAT_GENOME_LIB="GRCh37_gencode_v19_CTAT_lib_Nov012017"
 
 CTAT_GENOME_LIB_URL="https://data.broadinstitute.org/Trinity/CTAT_RESOURCE_LIB/${CTAT_GENOME_LIB}.plug-n-play.tar.gz"
 
@@ -13,7 +13,7 @@ fi
 
 
 if [ ! -d "../${CTAT_GENOME_LIB}" ]; then
-    tar xvf "../${CTAT_GENOME_LIB}.tar.gz -C ../."
+    tar xvf ../${CTAT_GENOME_LIB}.tar.gz -C ../.
 fi
 
 
@@ -22,5 +22,5 @@ fi
 VERSION=`cat VERSION.txt`
 
 # run STAR-Fusion
-docker run -v `pwd`/../:/data --rm trinityctat/ctatfusion:${VERSION} /usr/local/src/STAR-Fusion_v${VERSION}/STAR-Fusion --left_fq /data/testing/reads_1.fq.gz --right_fq /data/testing/reads_2.fq.gz --genome_lib_dir /data/${CTAT_GENOME_LIB}/ctat_genome_lib_build_dir -O /data/testing/test_docker_outdir/StarFusionOut --FusionInspector inspect --annotate --examine_coding_effect --denovo_reconstruct
+docker run -v `pwd`/../:/data --rm trinityctat/ctatfusion:${VERSION} /usr/local/src/STAR-Fusion-v${VERSION}/STAR-Fusion --left_fq /data/testing/reads_1.fq.gz --right_fq /data/testing/reads_2.fq.gz --genome_lib_dir /data/${CTAT_GENOME_LIB}/ctat_genome_lib_build_dir -O /data/testing/test_docker_outdir/StarFusionOut --FusionInspector inspect --annotate --examine_coding_effect --denovo_reconstruct
 
