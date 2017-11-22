23a936608d5e065368bcc02b92c954a63ff05b0d
Brian Haas <brianjohnhaas@gmail.com>
2017-11-22 12:27:14 -0500

    if --min_junction_reads 0, auto set --require_LDAS 0

--- util/STAR-Fusion.run_many_samples.pl
+++ util/STAR-Fusion.run_many_samples.pl
@@ -9,14 +9,14 @@ use Process_cmd;
 use File::Basename;
 use Getopt::Long qw(:config posix_default no_ignore_case bundling pass_through);
 
-my $usage = "\n\n\tusage: $0 samples.txt num_parallel [starF options passthru] [--skip_remove]\n\n";
+my $usage = "\n\n\tusage: $0 samples.txt num_parallel [starF options passthru] [--do_remove]\n\n";
 
 if (! $ENV{CTAT_GENOME_LIB}) {
     die "Error, no env var set for CTAT_GENOME_LIB";
 }
 
-my $skip_remove;
-&GetOptions ( 'skip_remove' => \$skip_remove);
+my $do_remove;
+&GetOptions ( 'do_remove' => \$do_remove);
 
 my $samples_file = $ARGV[0] or die $usage;
 my $num_parallel = $ARGV[1] or die $usage;
@@ -65,8 +65,8 @@ main: {
     &process_cmd($parafly_cmd);
 
 
-    unless ($skip_remove) {
-        
+    if ($do_remove) {
+         
         # unload the genome from memory
         $cmd = "$star_fusion_prog  --STAR_Remove ";
         &process_cmd($cmd);
