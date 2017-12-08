28224fe6e4bde47684c6c3a3167b4743608451ef
Brian Haas <brianjohnhaas@gmail.com>
2017-12-08 12:48:09 -0500

    samples file integration

--- util/get_FUSION_EVIDENCE_fastqs.pl
+++ util/get_FUSION_EVIDENCE_fastqs.pl
@@ -18,9 +18,15 @@ my $usage = <<__EOUSAGE__;
 #
 #  --fusions <string>           fusion predictions 'final' output file (not abridged)
 #
-#  --left_fq <string>           /1 fastq file (or SE reads)
+#  Reads:
 #
-#  --right_fq <string>          /2 fastq file (optional, required for PE reads)
+#     --left_fq <string>           /1 fastq file (or SE reads)
+#
+#     --right_fq <string>          /2 fastq file (optional, required for PE reads)
+#
+#   or
+#     --samples_file <string>
+#
 #
 #  --output_prefix <string>     output prefix
 #
@@ -37,7 +43,7 @@ my $fusion_results_file;
 my $left_fq;
 my $right_fq;
 my $output_prefix;
-
+my $samples_file;
 
 &GetOptions( 'help|h' => \$help_flag,
 
@@ -45,6 +51,9 @@ my $output_prefix;
              
              'left_fq=s' => \$left_fq,
              'right_fq=s' => \$right_fq,
+             
+             'samples_file=s' => \$samples_file,
+
              'output_prefix=s' => \$output_prefix,
     );
 
@@ -52,7 +61,7 @@ if ($help_flag) {
     die $usage;
 }
 
-unless ($fusion_results_file && $left_fq && $output_prefix) {
+unless ($fusion_results_file && ($left_fq  || $samples_file) && $output_prefix) {
     die $usage;
 }
 
@@ -67,10 +76,13 @@ main: {
 
     while (my $row = $tab_reader->get_row()) {
         my $fusion_name = $row->{'#FusionName'} or die "Error, cannot get fusion name from " . Dumper($row);
-
+        
         my $junction_reads_list_txt = $row->{JunctionReads};
         
         foreach my $junction_read (split(/,/, $junction_reads_list_txt)) {
+
+            $junction_read =~ s/^\&[^\@]+\@//; # remove any sample encoding here.
+            
             my $pair_end = "";
             if ($junction_read =~ /^(\S+)\/([12])$/) {
                 $junction_read = $1;
@@ -87,11 +99,18 @@ main: {
         
         my $spanning_frag_list_txt = $row->{SpanningFrags};
         foreach my $spanning_frag (split(/,/, $spanning_frag_list_txt)) {
+            
+            $spanning_frag =~ s/^\&[^\@]+\@//; # remove any sample encoding here.
             my $updated_frag_name = "$fusion_name|S|$spanning_frag";
             $core_frag_name_to_fusion_name{$spanning_frag} = $updated_frag_name;
         }
     }
-        
+
+
+    if ($samples_file) {
+        ($left_fq, $right_fq) = &parse_samples_file($samples_file);
+    }
+    
     &write_fastq_files($left_fq, "$output_prefix.fusion_evidence_reads_1.fq", \%core_frag_name_to_fusion_name);
     
     if ($right_fq) {
@@ -107,26 +126,31 @@ main: {
 
 ####
 sub write_fastq_files {
-    my ($input_fastq_file, $output_fastq_file, $core_frag_name_to_fusion_name_href) = @_;
-    
-    my $fastq_reader = new Fastq_reader($input_fastq_file);
+    my ($input_fastq_files, $output_fastq_file, $core_frag_name_to_fusion_name_href) = @_;
 
     open (my $ofh, ">$output_fastq_file") or die "Error, cannot write to $output_fastq_file";
     
-    while (my $fq_record = $fastq_reader->next()) {
+    foreach my $input_fastq_file (split(/,/, $input_fastq_files)) {
         
-        my $core_read_name = $fq_record->get_core_read_name();
-        #print "[$core_read_name]\n";
+        my $fastq_reader = new Fastq_reader($input_fastq_file);
         
-        if (my $fusion_name = $core_frag_name_to_fusion_name_href->{$core_read_name}) {
-
-            my $record_text = $fq_record->get_fastq_record();
-            chomp $record_text;
-            my @lines = split(/\n/, $record_text);
-            my ($_1, $_2, $_3, $_4) = @lines;
-            $_3 = "+$fusion_name"; # encode the fusion name in the 3rd line, which is otherwise useless anyway
-            print $ofh join("\n", ($_1, $_2, $_3, $_4)) . "\n";
+        while (my $fq_record = $fastq_reader->next()) {
+            
+            my $core_read_name = $fq_record->get_core_read_name();
+            #print "[$core_read_name]\n";
+            
+            if (my $fusion_name = $core_frag_name_to_fusion_name_href->{$core_read_name}) {
+                
+                my $record_text = $fq_record->get_fastq_record();
+                chomp $record_text;
+                my @lines = split(/\n/, $record_text);
+                my ($_1, $_2, $_3, $_4) = @lines;
+                $_3 = "+$fusion_name"; # encode the fusion name in the 3rd line, which is otherwise useless anyway
+                print $ofh join("\n", ($_1, $_2, $_3, $_4)) . "\n";
+            }
         }
+        
+        
     }
     
     print STDERR "\nDone writing to $output_fastq_file\n\n";
@@ -163,3 +187,28 @@ sub parse_core_frag_names {
 }
 
 
+####
+sub parse_samples_file {
+    my ($samples_file) = @_;
+
+    my @left_fqs;
+    my @right_fqs;
+    
+    open(my $fh, $samples_file) or die "Error, cannot open file: $samples_file";
+    while (<$fh>) {
+        chomp;
+        my @x = split(/\t/);
+        my ($sample_name, $left_fq, $right_fq) = @x;
+
+        push (@left_fqs, $left_fq);
+        if ($right_fq) {
+            push (@right_fqs, $right_fq);
+        }
+    }
+
+    my $left_fq_files = join(",", @left_fqs);
+    my $right_fq_files = join(",", @right_fqs);
+
+
+    return($left_fq_files, $right_fq_files);
+}
