7d8291219db6387f54986873699fd082540ef5a7
Brian Haas <brianjohnhaas@gmail.com>
2018-02-08 17:18:11 -0500

    add

---
+++ util/misc/samples_file_to_starF_cmds.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use FindBin;
+
+my $usage = "\n\n\tusage: $0 samples.txt\n\n";
+
+my $samples_file = $ARGV[0] or die $usage;
+
+main: {
+
+    open(my $fh, $samples_file) or die "error, cannot open file $samples_file";
+    while(<$fh>) {
+        chomp;
+        my ($sample_name, $left_fq, $right_fq) = split(/\s+/);
+
+        my $cmd = "$FindBin::Bin/../../STAR-Fusion --left_fq $left_fq --right_fq $right_fq --output_dir $sample_name --bbmerge";
+
+        print "$cmd\n";
+
+    }
+    close $fh;
+
+    exit(0);
+}
+
+
+        
+
+ 
+    
