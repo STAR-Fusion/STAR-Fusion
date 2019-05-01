089b11a0a3aeeddf24c14fc697f9d67ae9eb067d
Brian Haas <brianjohnhaas@gmail.com>
2019-04-30 21:01:52 -0400

    count remaining too

--- util/misc/audit_read_and_fusion_filtering.pl
+++ util/misc/audit_read_and_fusion_filtering.pl
@@ -11,6 +11,25 @@ my $chimJ_file = $ARGV[0] or die $usage;
 my $star_fusion_outdir = $ARGV[1] or die $usage;
 
 
+my $count_sub = sub {
+    my ($file) = @_;
+    
+    print STDERR "count_FFPM_filtered() - parsing $file\n";
+    
+    open(my $fh, $file) or confess "Error, cannot open file: $file";
+    my $header = <$fh>;
+    my %fusions;
+    while(<$fh>) {
+        my @x = split(/\t/);
+        my $fusion = $x[0];
+        $fusions{$fusion}++;
+    }
+        close $fh;
+    
+    return(scalar(keys %fusions));
+};
+
+
 main: {
      
      my %audit;
@@ -25,23 +44,31 @@ main: {
      ## applied basic filtering criteria (ie. min support)
      &count_pre_blast_filt("$star_fusion_outdir/star-fusion.preliminary/star-fusion.filter.intermediates_dir/star-fusion.pre_blast_filter.filt_info", \%audit);
 
+     my $total_remaining_after_basic_filtering = &$count_sub("$star_fusion_outdir/star-fusion.preliminary/star-fusion.filter.intermediates_dir/star-fusion.pre_blast_filter");
      
      ## remove those that are lesser scored paralogs of fusion partners
      &count_blast_filt("$star_fusion_outdir/star-fusion.preliminary/star-fusion.filter.intermediates_dir/star-fusion.pre_blast_filter.post_blast_filter.info", \%audit);
-
+     $audit{'remaining_post_blast_filt'} = &$count_sub("$star_fusion_outdir/star-fusion.preliminary/star-fusion.filter.intermediates_dir/star-fusion.pre_blast_filter.post_blast_filter");
+     
+     
      
      &count_promiscuous_filt("$star_fusion_outdir/star-fusion.preliminary/star-fusion.filter.intermediates_dir/star-fusion.pre_blast_filter.post_blast_filter.post_promisc_filter.info", \%audit);
-
+     $audit{'remaining_post_promisc_filt'} = &$count_sub("$star_fusion_outdir/star-fusion.preliminary/star-fusion.filter.intermediates_dir/star-fusion.pre_blast_filter.post_blast_filter.post_promisc_filter");
+     
+     
 
      &count_red_herrings("$star_fusion_outdir/star-fusion.preliminary/star-fusion.fusion_candidates.preliminary.wSpliceInfo.wAnnot.annot_filt", \%audit);
-
+     $audit{'remaining_post_red_herrings'} = &$count_sub("$star_fusion_outdir/star-fusion.preliminary/star-fusion.fusion_candidates.preliminary.wSpliceInfo.wAnnot.pass");
+     
+     
      &count_FFPM_filtered("$star_fusion_outdir/star-fusion.preliminary/star-fusion.fusion_candidates.preliminary.wSpliceInfo.wAnnot.pass",
                           "$star_fusion_outdir/star-fusion.preliminary/star-fusion.fusion_candidates.preliminary.wSpliceInfo.wAnnot.pass.minFFPM.0.1.pass",
                           \%audit);
+
+     
+     my $total_final_fusions = &$count_sub("$star_fusion_outdir/star-fusion.fusion_predictions.abridged.tsv");
      
      
-
-
      my $report = "";
      
      ## starting read counts:
@@ -83,61 +110,56 @@ main: {
      # 'pre_blast::low_pct_isoform' => 1
 
      $report .= "# basic filtering criteria applied.\n"
-         . "insufficient sum support:\t" . $audit{'pre_blast::insuf_sum_support'} . "\n"
-         . "insufficient novel junction support:\t" . $audit{'pre_blast::insuf_novel_junc_support'} . "\n"
-         . "no junction support:\t" . $audit{'pre_blast::no_junction_support'} . "\n"
-         . "no span, no LDAS:\t" . $audit{'pre_blast::no_span_no_LDAS'} . "\n"
-         . "low pct isoform:\t" . $audit{'pre_blast::low_pct_isoform'} . "\n"
+         . "insufficient sum support:\t" . $audit{'pre_blast::insuf_sum_support'} . sprintf("\t%.2f", $audit{'pre_blast::insuf_sum_support'}/$audit{'prelim_fusion_count'} * 100) . "\n"
+         . "insufficient novel junction support:\t" . $audit{'pre_blast::insuf_novel_junc_support'} . sprintf("\t%.2f", $audit{'pre_blast::insuf_novel_junc_support'}/$audit{'prelim_fusion_count'} * 100) . "\n"
+         . "no junction support:\t" . $audit{'pre_blast::no_junction_support'} . sprintf("\t%.2f", $audit{'pre_blast::no_junction_support'}/$audit{'prelim_fusion_count'} * 100) . "\n"
+         . "no span, no LDAS:\t" . $audit{'pre_blast::no_span_no_LDAS'} . sprintf("\t%.2f", $audit{'pre_blast::no_span_no_LDAS'} / $audit{'prelim_fusion_count'}  * 100) . "\n"
+         . "low pct isoform:\t" . $audit{'pre_blast::low_pct_isoform'} . sprintf("\t%.2f", $audit{'pre_blast::low_pct_isoform'} / $audit{'prelim_fusion_count'} * 100) .  "\n";
+
+
+     
+     my $total_basic_filtered = $audit{'prelim_fusion_count'} - $total_remaining_after_basic_filtering;
+
+     $report .= "-------------------------------------------\n"
+         . "total basic filtered:\t" . $total_basic_filtered . sprintf("\t%.2f", $total_basic_filtered / $audit{'prelim_fusion_count'} * 100) . "\n"
          . "\n";
      
-
-
+     $report .= "\tnum fusion candidates remaining:\t$total_remaining_after_basic_filtering\n\n";
+     
+     
      $report .= "# Final feature filters.\n"
          ## blast filter   A--B exists, removing C--B where A,C are paralogs and score(C--B) < score(A--B) 
          # 'blast_filt' => 27,
          
-         . "blast paralog filter:\t" . $audit{'blast_filt'} . "\n"
+         . "blast paralog filter:\t" . $audit{'blast_filt'} . ", remaining post blast filt: " . $audit{'remaining_post_blast_filt'} . "\n"
          
          ## promiscuity filter
          # 'promisc_filt' => 4, 
-         . "promiscuity filter:\t" . $audit{'promisc_filt'} . "\n"
+         . "promiscuity filter:\t" . $audit{'promisc_filt'} . ", remaining post promisc filt: " . $audit{'remaining_post_promisc_filt'} . "\n"
          
          ## red herring annotation filter:
          ## 'red_herrings_filt' => 27,
-         . "red herrings filter:\t" . $audit{'red_herrings_filt'} . "\n"
-     
+         . "red herrings filter:\t" . $audit{'red_herrings_filt'} . ", remaining post red herring filt: " . $audit{'remaining_post_red_herrings'} . "\n"
+         
          ## final expression filter
          # 'FFPM_filt' => 5,
          . "FFPM filter:\t" . $audit{'FFPM_filt'} . "\n"
 
          . "\n";
 
+     $report .= "Final fusions called:\t" . $total_final_fusions . "\n\n";
+     
 
      print $report;
      
 }
 
+
+
 ####
 sub count_FFPM_filtered {
     my ($file_before, $file_after, $audit_href) = @_;
         
-    my $count_sub = sub {
-        my ($file) = @_;
-
-        print STDERR "count_FFPM_filtered() - parsing $file\n";
-        
-        open(my $fh, $file) or confess "Error, cannot open file: $file";
-        my $header = <$fh>;
-        my %fusions;
-        while(<$fh>) {
-            my @x = split(/\t/);
-            my $fusion = $x[0];
-            $fusions{$fusion}++;
-        }
-        close $fh;
-
-        return(scalar(keys %fusions));
-    };
 
     my $count_before = &$count_sub($file_before);
     my $count_after = &$count_sub($file_after);
@@ -347,7 +369,8 @@ sub count_pre_blast_filt {
         }
         
         $audit_href->{"pre_blast::$token"}++;
+        $seen{$fusion}=1;
     }
-            
+    
     return;
 }
