aeb297663f3798ee0b1db2dee847de0d03357d9d
brianjohnhaas <brianjohnhaas@gmail.com>
2019-06-19 15:31:18 +0000

    filename update

--- Docker/__private/build_push_makeSIMG.sh
+++ Docker/__private/build_push_makeSIMG.sh
@@ -6,5 +6,5 @@ docker build -t localhost:6000/trinctatfusion:devel .
 
 docker push localhost:6000/trinctatfusion:devel
 
-sudo SINGULARITY_NOHTTPS=1 singularity build starfusion.simg docker://localhost:6000/trinctatfusion:devel
+sudo SINGULARITY_NOHTTPS=1 singularity build starfusion.devel.simg docker://localhost:6000/trinctatfusion:devel
 
