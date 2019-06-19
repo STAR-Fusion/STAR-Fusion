e893a66daa210ae543b6d61d976f46173e761d55
brianjohnhaas <brianjohnhaas@gmail.com>
2019-06-19 15:20:23 +0000

    adding devel simg builder

---
+++ Docker/__private/build_push_makeSIMG.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ev
+
+docker build -t localhost:6000/trinctatfusion:devel .
+
+docker push localhost:6000/trinctatfusion:devel
+
+sudo SINGULARITY_NOHTTPS=1 singularity build starfusion.simg docker://localhost:6000/trinctatfusion:devel
+
