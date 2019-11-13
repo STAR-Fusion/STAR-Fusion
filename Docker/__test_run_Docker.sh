2c63e66b52ee3ce0737a23a858d7bddc1bd0ffc7
brianjohnhaas <brianjohnhaas@gmail.com>
2019-11-13 14:52:05 +0000

    test makefile update

--- Docker/__test_run_Docker.sh
+++ Docker/__test_run_Docker.sh
@@ -2,24 +2,14 @@
 
 set -ve
 
-CTAT_GENOME_LIB="GRCh37_gencode_v19_CTAT_lib_Aug152019.plug-n-play"
-
-
-CTAT_GENOME_LIB_URL="https://data.broadinstitute.org/Trinity/CTAT_RESOURCE_LIB/GRCh37_gencode_v19_CTAT_lib_Aug152019.plug-n-play.tar.gz"
-
-
-if [ ! -s "../${CTAT_GENOME_LIB}.tar.gz" ] && [ ! -d "../${CTAT_GENOME_LIB}" ]; then
-    wget ${CTAT_GENOME_LIB_URL} -O ../${CTAT_GENOME_LIB}.tar.gz
-fi
-
-
-if [ ! -d "../${CTAT_GENOME_LIB}/" ]; then
-    tar xvf ../${CTAT_GENOME_LIB}.tar.gz -C ../.
+if [ -z ${CTAT_GENOME_LIB} ]; then
+    echo "Error, must have CTAT_GENOME_LIB env var set"
+    exit 1
 fi
 
 
 VERSION=`cat VERSION.txt`
 
 # run STAR-Fusion
-docker run -v `pwd`/../:/data --rm trinityctat/starfusion:${VERSION} /usr/local/src/STAR-Fusion/STAR-Fusion --left_fq /data/testing/reads_1.fq.gz --right_fq /data/testing/reads_2.fq.gz --genome_lib_dir /data/${CTAT_GENOME_LIB}/ctat_genome_lib_build_dir -O /data/testing/test_docker_outdir/StarFusionOut --FusionInspector inspect --examine_coding_effect --denovo_reconstruct
+docker run -v `pwd`/../:/data -v ${CTAT_GENOME_LIB}:/ctat_genome_lib --rm trinityctat/starfusion:${VERSION} /usr/local/src/STAR-Fusion/STAR-Fusion --left_fq /data/testing/reads_1.fq.gz --right_fq /data/testing/reads_2.fq.gz --genome_lib_dir /ctat_genome_lib -O /data/testing/test_docker_outdir/StarFusionOut --FusionInspector inspect --examine_coding_effect --denovo_reconstruct
 
--- Docker/__test_run_Singularity.sh
+++ Docker/__test_run_Singularity.sh
@@ -2,15 +2,13 @@
 
 set -ve
 
-## assumes we already ran/tested the Docker image
-
-
-
-CTAT_GENOME_LIB="GRCh37_gencode_v19_CTAT_lib_Aug152019.plug-n-play"
-
+if [ -z ${CTAT_GENOME_LIB} ]; then
+    echo "Error, must have CTAT_GENOME_LIB env var set"
+    exit 1
+fi
 
 VERSION=`cat VERSION.txt`
 
 # run STAR-Fusion
-cd ../ && singularity exec Docker/star-fusion.v${VERSION}.simg  /usr/local/src/STAR-Fusion/STAR-Fusion --left_fq testing/reads_1.fq.gz --right_fq testing/reads_2.fq.gz --genome_lib_dir ${CTAT_GENOME_LIB}/ctat_genome_lib_build_dir -O testing/test_singularity_outdir/StarFusionOut --FusionInspector inspect --examine_coding_effect --denovo_reconstruct
+cd ../ && singularity exec -e -B ${CTAT_GENOME_LIB}:/ctat_genome_lib Docker/star-fusion.v${VERSION}.simg  /usr/local/src/STAR-Fusion/STAR-Fusion --left_fq testing/reads_1.fq.gz --right_fq testing/reads_2.fq.gz --genome_lib_dir /ctat_genome_lib -O testing/test_singularity_outdir/StarFusionOut --FusionInspector inspect --examine_coding_effect --denovo_reconstruct
 
